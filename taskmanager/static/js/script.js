d11eaf15ad249bf3c36d1d945613c64831166fbf
Richard Messenger <rchrdmessenger@gmail.com>
2024-02-27 13:31:04 +0000

    Tasks part 1

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,33 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-  });
+
+    // Datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Select Initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+});
+
+
+  // Initialize modal-trigger buttons
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal, {});
+
+  // Below courtesy of Chat GPT
+
+  let modalTriggerButtons = document.querySelectorAll('.modal-trigger');
+  modalTriggerButtons.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+      event.preventDefault();
+      let target = btn.getAttribute('href');
+      let modalInstance = M.Modal.getInstance(document.querySelector(target));
+      modalInstance.open();
+    });
+
+});
