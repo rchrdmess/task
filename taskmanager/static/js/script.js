dce907488f34f7f66050344cfdef21c8faceaf7d
Richard Messenger <rchrdmessenger@gmail.com>
2024-02-27 17:43:59 +0000

    Last vid

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
+
     // Datepicker initialization
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
@@ -10,25 +11,30 @@ document.addEventListener("DOMContentLoaded", function() {
         i18n: {done: "Select"}
     });
 
+
     // Select Initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
-});
 
 
-  // Initialize modal-trigger buttons
-  let modal = document.querySelectorAll('.modal');
-  M.Modal.init(modal, {});
+    // Collapsible Initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 
-  // Below courtesy of Chat GPT
 
-  let modalTriggerButtons = document.querySelectorAll('.modal-trigger');
-  modalTriggerButtons.forEach(function(btn) {
-    btn.addEventListener('click', function(event) {
-      event.preventDefault();
-      let target = btn.getAttribute('href');
-      let modalInstance = M.Modal.getInstance(document.querySelector(target));
-      modalInstance.open();
+    // Initialize modal-trigger buttons
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal, {});
+
+    // Below courtesy of Chat GPT
+
+    let modalTriggerButtons = document.querySelectorAll('.modal-trigger');
+    modalTriggerButtons.forEach(function(btn) {
+      btn.addEventListener('click', function(event) {
+        event.preventDefault();
+        let target = btn.getAttribute('href');
+        let modalInstance = M.Modal.getInstance(document.querySelector(target));
+        modalInstance.open();
+      });
     });
-
 });
