3e421787dba215390a0a93430d24a4849921c526
Richard Messenger <rchrdmessenger@gmail.com>
2024-02-29 16:49:25 +0000

    Update

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
+    else:
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
